bd24381efb9abb89d2fc0ac12458ffdd7eb67dc4
adingara-ship-it <adingara@gmail.com>
2026-01-01 15:35:14 +0100

    Passage en mode SSR automatique

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,14 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://clubcolak.be', // J'ai mis ton URL probable
+    output: 'server', // Active le mode dynamique (SSR)
+    adapter: vercel({
+        webAnalytics: { enabled: true }
+    }),
+    integrations: [mdx(), sitemap()],
 });
