21b1b5fd3d6d47d357e9e873c739c90d000ae844
adingara-ship-it <adingara@gmail.com>
2026-01-03 12:30:04 +0100

    Fix sitemap site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,10 +7,10 @@ import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://clubcolak.be', // J'ai mis ton URL probable
-    output: 'server', // Active le mode dynamique (SSR)
-    adapter: vercel({
-        webAnalytics: { enabled: true }
-    }),
-    integrations: [mdx(), sitemap()],
+  site: 'https://www.boxingclubcolak.com',
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
+  integrations: [mdx(), sitemap()],
 });
